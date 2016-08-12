c057a9d09c4d2983fe40bd28e284eabc2441d2da
Dan Jenner <danjenner@gmail.com>
2016-08-12 10:43:28 -0500

    Code rdy for review, all requirements completed.

--- js/app.js
+++ js/app.js
@@ -1,43 +1,98 @@
 'use strict';
 
-var drinkIngredients = {
-    strong: ["Rich", "glug of rum", "slug of whiskey", "splash of gin", "flask of tequila"],
-    salty: ["Briny", "olive on a stick", "eye of newt", "salt-dusted rim", "rasher of bacon"],
-    bitter: ["Sour", "shake of bitters", "splash of tonic", "twist of lemon", "dash of tang"],
-    sweet: ["Luscious", "sugar cube", "spoonful of honey", "splash of cola", "sprinkle of cinnamon"],
-    fruity: ["Savory", "slice of orange", "dash of cassis", "cherry on top", "swirl of cream"]
+var drinkContainer = {
+    container: ["Flask", "Flagon", "Cask", "Mug"]
+}
+
+//bilds the drinkIngredients constructor function
+function drinkIngredients() {
+    this.strong = ["glug of rum", "slug of whiskey", "splash of gin", "shot of tequila"];
+    this.salty = ["olive on stick", "eye of newt", "rim of salt", "rasher of bacon"];
+    this.bitter = ["shake of bitters", "splash of tonic", "twist of lemon", "dash of tang"];
+    this.sweet = ["cube of sugar", "spoonful of honey", "splash of cola", "sprinkle of cinnamon"];
+    this.fruity = ["slice of orange", "dash of cassis", "sack of cherries", "swirl of cream"];
+    this.spicy = ["shake of paprika", "slice of jalepeno", "squirt of siracha", "grind of ginger"];
+}
+
+//drinkIngredients prototype methods
+//
+//makeDrink() = composes the drink out of ingredients and selections of 'yes' or 'no'
+//from the input form.  Take a random number, uses it to choose an ingredient from above
+//then builds the drink out of those ingredients
+//
+//displayOrder() = checks if the user made selections from the form, and alerts if they didn't
+//builds order variable then changes css to display result box and appends order for display
+//
+
+drinkIngredients.prototype = {
+    makeDrink: function () {
+        var order = '';
+        for (var prop in drinkIngredients) {
+            var random = Math.floor((Math.random() * 4));
+            if (!this.propertyIsEnumerable(prop)) {} else {
+                if (drinkOrder[prop] == 'no') {
+                    //do nothing, user chose not to include this in their drink
+                } else {
+                    order += "<li>Ingredients: " + drinkIngredients[prop][random] + '</li>';
+                    var container = drinkContainer.container[random];
+                }
+            }
+        }
+        this.displayOrder(container, order);
+    },
+
+    displayOrder: function (container, order) {
+        $('div.results').css('display', 'inline-block');
+        if (order == '') {
+            order = "<h2>Choose some ingredients you landlubber!</h2>";
+            $('div.results').append(order);
+        } else {
+            $('div.results').append('<hr><h2>Here\'s your Drink Matey in your ' + container + '</h2><ul>' + order + '</ul>');
+        }
+    }
 };
 
-var drinkOrder; //make a constructor function here
+//builds constructor function for the drinkOrder, will contain the form results
+function drinkOrder() {
+    this.strong = '';
+    this.salty = '';
+    this.bitter = '';
+    this.sweet = '';
+    this.fruity = '';
+    this.spicy = '';
+}
+
+//drinkOrder prototype methods
+//
+//checkDrink() = fills the drinkOrder object with 'yes' or 'no' depending on the user's choices on form
+//
+drinkOrder.prototype = {
+    checkDrink: function (event) {
+        this.strong = $('#strong option:selected').val();
+        this.salty = $('#salty option:selected').val();
+        this.bitter = $('#bitter option:selected').val();
+        this.sweet = $('#sweet option:selected').val();
+        this.fruity = $('#fruity option:selected').val();
+        this.spicy = $('#spicy option:selected').val();
+    }
+};
+
+//new drinking objects
+var drinkIngredients = new drinkIngredients();
+var drinkOrder = new drinkOrder();
 
 $(document).ready(function () {
     $('#drink_form').submit(function (event) {
+        //prevent form submission, use JS instead
         event.preventDefault();
 
-        //get the form data
-        var strong_choice = $('#strong option:selected').val();
-        var salty_choice = $('#salty option:selected').val();
-        var bitter_choice = $('#bitter option:selected').val();
-        var sweet_choice = $('#sweet option:selected').val();
-        var fruity_choice = $('#fruity option:selected').val();
+        //empty the results container for each form submission
+        $('div.results').empty();
 
-        console.log(strong_choice, salty_choice, bitter_choice, sweet_choice, fruity_choice);
+        //get the form data, call checkDrink to set with form results
+        drinkOrder.checkDrink(event);
 
-        //push all the yes/no data into an array, should end up with something like:
-        //[yes,no,no,yes,yes]
-        //
-        //for each yes, generate a random number from 1 to the length of
-        //the object name (drinkIngredients.strong for example)
-        //then make an array with a random number for each yes, for example:
-        //[3,no,no,2,5]
-        //then, loop through the drinkIngredients obj, getting the value that matches
-        //the number.
-        //
-        //then, build a drink using the first word in the name obj array
-        //display ingredients and the name, example:
-        //Rich Briny Grog
-
-
+        //match order with drink ingredients, build the drink, display the results
+        drinkIngredients.makeDrink();
     });
-
 });
